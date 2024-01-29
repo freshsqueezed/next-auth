2bca499fca6da52bb3e598814dc70a2c4588c282
matthew-gordon <m.gordon5525@gmail.com>
2024-01-28 17:37:54 -0700

    Add next auth credentials provider, custom config, custom middleware

--- auth.config.ts
+++ auth.config.ts
@@ -1,7 +1,29 @@
-import Github from 'next-auth/providers/github';
-
 import type { NextAuthConfig } from 'next-auth';
+import Credentials from 'next-auth/providers/credentials';
+
+import { LoginSchema } from '@/schemas';
+import { getUserByEmail } from './lib/db/queries/user';
+import { compare } from 'bcryptjs';
 
 export default {
-  providers: [Github],
+  providers: [
+    Credentials({
+      async authorize(credentials, req) {
+        const validateFields = LoginSchema.safeParse(credentials);
+
+        if (validateFields.success) {
+          const { email, password } = validateFields.data;
+
+          const user = await getUserByEmail(email);
+          if (!user || !user.password) return null;
+
+          const passwordMatch = await compare(password, user.password);
+          if (passwordMatch) return user;
+
+          return null;
+        }
+        return null;
+      },
+    }),
+  ],
 } satisfies NextAuthConfig;
