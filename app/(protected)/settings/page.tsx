88c1e76ffc84ba4e53f1915bcc8ae48a6bb2e470
matthew-gordon <m.gordon5525@gmail.com>
2024-01-28 17:50:02 -0700

    Add logout button

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,7 +1,20 @@
-import { auth } from '@/auth';
+import { auth, signOut } from '@/auth';
 
 export default async function Settings() {
   const session = await auth();
 
-  return <div>{JSON.stringify(session)}</div>;
+  return (
+    <div>
+      {JSON.stringify(session)}
+      <form
+        action={async () => {
+          'use server';
+
+          await signOut();
+        }}
+      >
+        <button type="submit">Sign Out</button>
+      </form>
+    </div>
+  );
 }
